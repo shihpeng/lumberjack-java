282a537cbd745a197d36afb7201b2850d61576b8
Shih-Peng Lin <shihpeng.lin@gmail.com>
2014-10-07 00:59:58 +0800

    Refactor the code so that the works can be modeled as an Object's behavior.

--- src/main/java/org/lumberjack/Client.java
+++ src/main/java/org/lumberjack/Client.java
@@ -3,6 +3,10 @@ package org.lumberjack;
 import javax.net.ssl.*;
 import java.io.*;
 import java.nio.ByteBuffer;
+import java.security.GeneralSecurityException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,9 +24,15 @@ public class Client {
     public final static String PROTOCOL_VERSION = "1";
     public final static String DATA_FRAME_TYPE = "D";
     public final static String COMPRESS_FRAME_TYPE = "C";
+    public final static String WRITE_FRAME_TYPE = "W";
 
-    private final static String SERVER_HOST = "127.0.0.1";
-    private final static int SERVER_PORT = 5555;
+    private int sequence = 0;
+    private int lastAck = 0;
+    private int windowSize = 5000;
+
+    private SSLSocket socket;
+    private BufferedOutputStream out;
+    private BufferedInputStream in;
 
     // a TrustManager that accepts any certificate
     private final static TrustManager[] DUMMY_TRUST_MANAGER = new TrustManager[]{new X509TrustManager()
@@ -32,111 +42,64 @@ public class Client {
         public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {}
     }};
 
-    public Client(String host, int port, String sslCertificate, int windowSize){
-
+    public Client(String host, int port) throws GeneralSecurityException, IOException {
+        this(host, port, null, -1);
     }
 
-    public static void main(String[] args) {
+    public Client(String host, int port, String sslCertificate, int windowSize) throws GeneralSecurityException, IOException {
 
-        /*
-         * The message going to be sent.
-         */
-        String message = "{\"key1\": \"value1\"}";
+        SSLContext sslContext = SSLContext.getInstance("SSL");
+        sslContext.init(null, DUMMY_TRUST_MANAGER, new SecureRandom());
+        SSLSocketFactory factory = sslContext.getSocketFactory();
 
-        try {
-            /*
-             * Step 1. Connect to the Lumberjack server with proper SSL settings.
-             *         (The Lumberjack server is very likely to be a LogStash instance.)
-             */
-            SSLContext sslContext = SSLContext.getInstance("SSL");
-            sslContext.init(null, DUMMY_TRUST_MANAGER, new java.security.SecureRandom());
-            SSLSocketFactory factory = sslContext.getSocketFactory();
-            SSLSocket socket = (SSLSocket) factory.createSocket(SERVER_HOST, SERVER_PORT);
+        socket = (SSLSocket) factory.createSocket(host, port);
+        out = new BufferedOutputStream(socket.getOutputStream());
+        in = new BufferedInputStream(socket.getInputStream());
 
-            /*
-             * Step 2. Get the output/input streams from SSLSocket.
-             */
-            BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream(), 5120);
-            BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
-
-
-            /*
-             * Step 3. Prepare the data to be send.
-             *
-             * Note: One of the keys of the data sent to server MUST BE "line", the value of the key "line" will be the
-             *       main message parsed by the server.
-             */
-            Map<String, String> dataMap = new HashMap<String, String>();
-            dataMap.put("line", message);
-            dataMap.put("another_field", "data.of.another.field");
-
-            /*
-             * Step 3. Prepare the DATA frame.
-             * Note: Every message in Lumberjack protocol is called a "frame":
-             *       1. DATA frame: message that carries the real data to the Lumberjack server.
-             *       2. ACK frame: message sent from server to acknowledge receiving of previous DATA frames.
-             *       3. WINDOW SIZE frame: used to set the window size, which is the max number of unacknowledged DATA
-             *                             frames the client will send before blocking for acks.
-             *       4. COMPRESSED frame
-             */
-            int fakeSequenceNumber = 1; // maybe we can ignore this:)
-            byte[] dataFrame = prepareDataFrame(dataMap, fakeSequenceNumber);
-
-            /*
-             * Step 4a. Send the DATA frame.
-             */
-            //out.write(dataFrame);
-            //out.flush();
-
-             /*
-             * Step 4b. Compress several DATA frames to a COMPRESS FRAME then send.
-             */
-            byte[] anotherDataFrame = prepareDataFrame(dataMap, fakeSequenceNumber);
-            byte[][] dataFrames = new byte[][]{dataFrame, anotherDataFrame};
-            byte[] compressFrame = prepareCompressFrame(dataFrames);
-            out.write(compressFrame);
-            out.flush();
-
-            /*
-             * Step 5. Receive the ACK frame.
-             */
-            byte[] buff = new byte[1024];
-            int bytesReceived = in.read(buff);
-            String version = new String(Arrays.copyOfRange(buff, 0, 1));
-            String frameType = new String(Arrays.copyOfRange(buff, 1, 1));
-            int sequenceNumber = ByteBuffer.wrap(Arrays.copyOfRange(buff, 2, 6)).getInt();
-
-            // clean up
-            if(socket != null){
-                socket.close();
-            }
-        }
-        catch(Exception ex){
-            ex.printStackTrace();
-            System.err.print(ex.toString());
-            System.exit(1);
+        if(windowSize > 0){
+            this.windowSize = windowSize;
         }
     }
 
-    private static byte[] prepareDataFrame(Map<String, String> dataMap, int sequenceNumber) throws IOException {
+    public void write(Map dataMap) throws IOException{
 
-        /*
-         *  Sample:
-         *      dataMap = {"line": "a"}
-         *
-         *  Result data frame (a byte array):
-         *      {(byte)0x31,  //  "1" (ASCII code)
-         *       (byte)0x44,  //  "D" (ASCII code)
-         *       (byte)0x0, (byte)0x0, (byte)0x0, (byte)0x1,  // sequence number (4-byte big-endian encoding)
-         *       (byte)0x0, (byte)0x0, (byte)0x0, (byte)0x1,  // number of data key-value pairs (4-byte big-endian encoding)
-         *       (byte)0x0, (byte)0x0, (byte)0x0, (byte)0x4,  // length of 1st key byte repr. (4-byte big-endian encoding)
-         *       (byte)0x6C, (byte)0x69, (byte)0x61, (byte)0x65,  // byte representation of the 1st key
-         *       (byte)0x0, (byte)0x0, (byte)0x0, (byte)0x1,  // length of 1st value byte repr. (4-byte big-endian encoding)
-         *       (byte)0x61    // byte representation of the 1st value
-         *       .... // length of n-th value byte repr. (4-byte big-endian encoding)
-         *       .... // byte representation of the n-th value
-         *      }
-         */
+        if(sequence+1 > SEQUENCE_MAX){
+            sequence = 0;
+        }
+        sequence += 1;
+
+        byte[] dataFrame = prepareDataFrame(dataMap, sequence);
+
+        if((sequence - (lastAck + 1)) >= windowSize) {
+            ack();
+        }
+
+        byte[] compressFrame = prepareCompressFrame(new byte[][]{ dataFrame });
+        out.write(compressFrame);
+        out.flush();
+    }
+
+    private void ack() throws IOException{
+
+        String version =  new String(new byte[]{(byte)in.read()});
+        String type =  new String(new byte[]{(byte)in.read()});
+
+        byte[] tmp = new byte[4];
+        in.read(tmp, 0, 4);
+        lastAck = ByteBuffer.wrap(tmp).getInt();
+
+        if((sequence - (lastAck + 1)) >= windowSize){
+            ack();
+        }
+    }
+
+    private void close() throws IOException{
+        if(socket != null){
+            socket.close();
+        }
+    }
+
+    private byte[] prepareDataFrame(Map<String, String> dataMap, int sequenceNumber) throws IOException {
 
         ByteArrayOutputStream bytesOutput = new ByteArrayOutputStream();
 
@@ -158,19 +121,13 @@ public class Client {
         return bytesOutput.toByteArray();
     }
 
-    private static byte[] prepareCompressFrame(byte[][] dataFrames) throws IOException {
+    private byte[] prepareCompressFrame(byte[][] dataFrames) throws IOException {
 
-        /*
-         * Collect all the DATA frames into one byte array.
-         */
         ByteArrayOutputStream bytesOutput = new ByteArrayOutputStream();
         for (byte[] frame : dataFrames) {
             bytesOutput.write(frame);
         }
 
-        /*
-         * Compress this aggregated byte array with ZLib level 6.
-         */
         byte[] output = new byte[5120];
         Deflater deflater = new Deflater(6);
         deflater.setInput(bytesOutput.toByteArray());
@@ -180,14 +137,29 @@ public class Client {
 
         bytesOutput.reset();
 
-        /*
-         * Compose the COMPRESS frame using the compressed payload.
-         */
         bytesOutput.write(PROTOCOL_VERSION.getBytes());
         bytesOutput.write(COMPRESS_FRAME_TYPE.getBytes());
         bytesOutput.write(ByteBuffer.allocate(4).putInt(compressedLength).array());
         bytesOutput.write(Arrays.copyOf(output, compressedLength));
 
         return bytesOutput.toByteArray();
+    }
+
+    public static void main(String[] args) {
+        try {
+            Client client = new Client("127.0.0.1", 5555, null, -1);
+
+            Map<String, String> dataMap = new HashMap<String, String>();
+            dataMap.put("line", "{\"key1\": \"value1\"}");
+            dataMap.put("another_field", "data.of.another.field");
+
+            client.write(dataMap);
+
+            client.close();
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+            System.exit(1);
+        }
     }
 }
